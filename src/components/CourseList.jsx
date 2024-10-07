7dd46543c750cf550578a46210cf43ea8913500d
WeiViv <100058931+WeiViv@users.noreply.github.com>
2024-10-07 17:01:04 -0500

    Task H: Add style to the cards

--- src/components/CourseList.jsx
+++ src/components/CourseList.jsx
@@ -1,11 +1,22 @@
-const CourseList = ({courses}) => (
-    <div>
+import React from 'react';
+import './CourseList.css';
+
+const CourseList = ({ courses }) => (
+    <div className='course-list'>
         {Object.values(courses).map((course) => (
-            <div key={course.number}>
-                <p>{course.term} CS {course.number}: {course.title}</p>
+            <div key={`${course.term}-${course.number}`} className="card m-1 p-2">
+                <div className="card-body">
+                    <h5 className="card-title">{course.term} CS {course.number}</h5>
+                    <p className="card-text">{course.title}</p>
+                    {/* New container for line and meets info */}
+                    <div className="course-info-divider">
+                        <hr className="divider-line" />
+                        <p className="card-text">{course.meets}</p>
+                    </div>
+                </div>
             </div>
         ))}
-    </div> 
+    </div>
 );
 
 export default CourseList;
