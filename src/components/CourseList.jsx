fddac5c5a398a639b74e92102660781078ffe5eb
WeiViv <100058931+WeiViv@users.noreply.github.com>
2024-10-08 22:04:31 -0500

    Task K: Course Selector

--- src/components/CourseList.jsx
+++ src/components/CourseList.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import './CourseList.css';
 
-const CourseList = ({ courses, selectedTerm }) => {
-    const filteredCourses = Object.values(courses).filter(
-        (course) => course.term === selectedTerm
-    );
+const CourseList = ({ courses, selectedTerm, selectedCourse, toggleSelectedCourse}) => {
+    const filteredCourses = Object.entries(courses).filter(([key, course]) => course.term === selectedTerm);
+
     return(
         <div className='course-list'>
-        {filteredCourses.map((course) => (
-            <div key={`${course.term}-${course.number}`} className="card m-1 p-2">
+        {filteredCourses.map(([key, course]) => (
+            <div 
+                key={key} 
+                className={`card m-1 p-2" ${selectedCourse.includes(key) ? 'selected' : ''}`}
+                onClick={() => toggleSelectedCourse(key)}
+            >
                 <div className="card-body">
                     <h5 className="card-title">{course.term} CS {course.number}</h5>
                     <p className="card-text">{course.title}</p>
--- src/components/TermPage.jsx
+++ src/components/TermPage.jsx
@@ -3,14 +3,19 @@ import TermSelector from './TermSelector';
 import CourseList from './CourseList';
 
 const TermPage = ({ courses }) => {
-  const [selectedTerm, setSelectedTerm] = useState("Fall"); // Default to "Fall"
-
-return (
-    <div>
-        <TermSelector selectedTerm={selectedTerm} setSelectedTerm={setSelectedTerm} />
-        <CourseList courses={courses} selectedTerm={selectedTerm} />
-    </div>
-);
+    const [selectedTerm, setSelectedTerm] = useState("Fall"); // Default to "Fall"
+    const [selectedCourse, setSelectedCourse] = useState([]);
+    const toggleSelectedCourse = (item) => setSelectedCourse(
+        selectedCourse.includes(item)
+        ? selectedCourse.filter(x => x !== item)
+        : [...selectedCourse, item]
+    );// store selected items in selected array
+    return (
+        <div>
+            <TermSelector selectedTerm={selectedTerm} setSelectedTerm={setSelectedTerm} />
+            <CourseList courses={courses} selectedTerm={selectedTerm} selectedCourse={selectedCourse} toggleSelectedCourse={toggleSelectedCourse} />
+        </div>
+    );
 };
 
 export default TermPage;
